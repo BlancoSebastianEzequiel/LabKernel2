c270dcb37bce0287c1a1092167cc2986b424d897
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-06 01:50:27 -0300

    Agrego boot.S y kern2.c funcionando donde se imprime por pantalla el mensaje deseado

--- kern2.c
+++ kern2.c
@@ -2,7 +2,5 @@
 #include "multiboot.h"
 void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
-    while (1) {
-        __asm__("hlt");
-    }
+    while (1) __asm__("hlt");
 }
