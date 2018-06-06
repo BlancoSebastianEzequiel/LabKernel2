86cd138d161b4d469bcaeba2ba1ec36313229351
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-06 00:55:19 -0300

    Agrego main del kernel

---
+++ kern2.c
@@ -0,0 +1,8 @@
+#include "decls.h"
+#include "multiboot.h"
+void kmain(const multiboot_info_t *mbi) {
+    vga_write("kern2 loading.............", 8, 0x70);
+    while (1) {
+        __asm__("hlt");
+    }
+}
