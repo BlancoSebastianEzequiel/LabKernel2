d06fe549006c01bf1b057e9e95ac7ca46c9a1e2f
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-06 17:21:14 -0300

    Agrego archivo principal con concatenacion funcionando

--- kern2.c
+++ kern2.c
@@ -1,6 +1,30 @@
 #include "decls.h"
 #include "multiboot.h"
+#include "lib/string.h"
+#include "lib/stddef.h"
+//------------------------------------------------------------------------------
+//  PRINT NUM
+//------------------------------------------------------------------------------
+void print_num(const size_t num, int8_t linea, uint8_t color) {
+    char size_char[256];
+    int decenas = num/10;
+    int unidades = num-(decenas*10);
+    size_char[0] = (char) (decenas + 48);
+    size_char[1] = (char) (unidades + 48);
+    vga_write(size_char, linea, color);
+}
+//------------------------------------------------------------------------------
+//  KMAIN
+//------------------------------------------------------------------------------
 void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
+    if (mbi->flags || 1) {
+        char buf[256] = "cmdline: ";
+        // char *cmdline = (void *) mbi->cmdline;
+        char cmdline[256] = "hola!!";
+        strlcat(buf, cmdline, strlen(buf)+strlen(cmdline)+1);
+        vga_write(buf, 9, 0x07);
+    }
     while (1) __asm__("hlt");
 }
+//------------------------------------------------------------------------------
