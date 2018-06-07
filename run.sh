f1acd8eb4bd76d2d20a90bf47ef84f61f7133994
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-07 17:37:53 -0300

    Agrego cambios en el archivo de ejecucion para poder correr Ej: kern2-meminfo

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 clear
 make clean
-make qemu QEMU_EXTRA='-append "param1=hola param2=adios"'
+#make qemu QEMU_EXTRA='-append "param1=hola param2=adios"'
+make qemu QEMU_EXTRA="-m 256"
 #make qemu-gdb QEMU_EXTRA='-append "param1=hola param2=adios"'
 make clean
