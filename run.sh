932c46eedee0674f5b92569462f7d8ca30f3448c
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-07 02:11:40 -0300

    Agrego kernel2 con modificaciones pequeñas

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 clear
 make clean
-make
-make qemu QEMU_EXTRA="-append 'param1=hola param2=adios'"
+#make qemu QEMU_EXTRA='-append "param1=hola param2=adios"'
+make qemu QEMU_EXTRA='-append "param=hola param=adios"'
 make clean
