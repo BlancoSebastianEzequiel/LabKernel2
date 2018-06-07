615373e15403a0b02f1237a4b0e8e7392c08917d
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-07 13:54:21 -0300

    Agrego cambios en el archivo de ejecucion

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 clear
 make clean
-make qemu QEMU_EXTRA='-append "param=hola param=adios"'
+make qemu QEMU_EXTRA='-append "param1=hola param2=adios"'
+#make qemu-gdb QEMU_EXTRA='-append "param1=hola param2=adios"'
 make clean
