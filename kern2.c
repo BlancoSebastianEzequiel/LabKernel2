908ac86814c48bbec5496a88c9766e4e2296f23d
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-07 02:10:58 -0300

    Agrego kernel2 con modificaciones pequeññas

--- kern2.c
+++ kern2.c
@@ -14,14 +14,43 @@ void print_num(const size_t num, int8_t linea, uint8_t color) {
     vga_write(size_char, linea, color);
 }
 //------------------------------------------------------------------------------
+//  DIGITS QUANTITY
+//------------------------------------------------------------------------------
+size_t digitsQuantity(const uint32_t num) {
+    uint32_t n = num;
+    size_t digits = 0;
+    while(n > 0) {
+        digits++;
+        n = n / 10;
+    }
+    return digits;
+}
+//------------------------------------------------------------------------------
+//  UINT32_T TO STRING
+//------------------------------------------------------------------------------
+void uint32_tToString(const uint32_t num, char* buf, size_t maxSize) {
+    uint32_t r;
+    size_t digits = digitsQuantity(num);
+    if (digits > maxSize) return;
+    uint32_t n = num;
+    size_t pos = digits;
+    buf[pos] = '\0';
+    while(n > 0) {
+        pos--;
+        r = n % 10;
+        buf[pos] = (char) (r + 48);
+        n = n / 10;
+    }
+}
+//------------------------------------------------------------------------------
 //  KMAIN
 //------------------------------------------------------------------------------
 void kmain(const multiboot_info_t *mbi) {
+    if (mbi == NULL) vga_write("mbi == NULL", 5, 0x70);
     vga_write("kern2 loading.............", 8, 0x70);
-    if (mbi->flags || 1) {
+    if (mbi->flags == MULTIBOOT_INFO_CMDLINE) {
         char buf[256] = "cmdline: ";
-        // char *cmdline = (void *) mbi->cmdline;
-        char cmdline[256] = "hola!!";
+        char *cmdline = (void*) mbi->cmdline;
         strlcat(buf, cmdline, strlen(buf)+strlen(cmdline)+1);
         vga_write(buf, 9, 0x07);
     }
