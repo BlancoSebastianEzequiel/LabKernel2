a3b11bb36421cd3f03045e9a2c72fd9bfa33d574
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-08 21:21:42 -0300

    Agrego el codigo del calculo de memoria

--- kern2.c
+++ kern2.c
@@ -3,40 +3,12 @@
 #include "lib/string.h"
 #include "lib/stddef.h"
 //------------------------------------------------------------------------------
-//  DIGITS QUANTITY
-//------------------------------------------------------------------------------
-size_t digitsQuantity(const uint32_t num) {
-    uint32_t n = num;
-    size_t digits = 0;
-    while(n > 0) {
-        digits++;
-        n = n / 10;
-    }
-    return digits;
-}
-//------------------------------------------------------------------------------
-//  UINT32_T TO STRING
-//------------------------------------------------------------------------------
-void uint32_tToString(const uint32_t num, char* buf, size_t maxSize) {
-    uint32_t r;
-    size_t digits = digitsQuantity(num);
-    if (digits > maxSize) return;
-    uint32_t n = num;
-    size_t pos = digits;
-    buf[pos] = '\0';
-    while(n > 0) {
-        pos--;
-        r = n % 10;
-        buf[pos] = (char) (r + 48);
-        n = n / 10;
-    }
-}
-//------------------------------------------------------------------------------
 //  KMAIN
 //------------------------------------------------------------------------------
 void kmain(const multiboot_info_t *mbi) {
     if (mbi == NULL) vga_write("mbi == NULL", 10, 0x70);
     vga_write("kern2 loading.............", 8, 0x70);
+
     if (mbi->flags & MULTIBOOT_INFO_CMDLINE) {
         char buf[256] = "cmdline: ";
         char *cmdline = (void *) mbi->cmdline;
@@ -46,13 +18,23 @@ void kmain(const multiboot_info_t *mbi) {
 
     char mem[256] = "Physical memory: ";
     char tmp[64] = "";
+    uint32_t memSize = mbi->mem_upper - mbi->mem_lower;
+    memSize = (memSize >> 10) + 1;
 
-    if (fmt_int(mbi->cmdline, tmp, sizeof tmp)) {
+    if (fmt_int(memSize, tmp, sizeof tmp)) {
         strlcat(mem, tmp, sizeof mem);
-        strlcat(mem, "MiB total", sizeof mem);
+        strlcat(mem, "MiB total (", sizeof mem);
+    }
+    if (fmt_int(mbi->mem_lower, tmp, sizeof tmp)) {
+        strlcat(mem, tmp, sizeof mem);
+        strlcat(mem, " base, ", sizeof mem);
+    }
+    if (fmt_int(mbi->mem_upper, tmp, sizeof tmp)) {
+        strlcat(mem, tmp, sizeof mem);
+        strlcat(mem, "KiB extended)", sizeof mem);
     }
     vga_write(mem, 10, 0x07);
 
-    while (1) __asm__("hlt");
+    while (1) asm("hlt");
 }
 //------------------------------------------------------------------------------
