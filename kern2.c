a77f1a5bdf24c90b1ebda15347c533a65ac0e7c7
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-09 17:09:58 -0300

    Ej: kern2-exec: Agrego funcion two_stacks_c() funcionando correctamente

--- kern2.c
+++ kern2.c
@@ -2,6 +2,58 @@
 #include "multiboot.h"
 #include "lib/string.h"
 #include "lib/stddef.h"
+
+#define USTACK_SIZE 4096
+static uint8_t stack1[USTACK_SIZE] __attribute__((aligned(4096)));
+static uint8_t stack2[USTACK_SIZE] __attribute__((aligned(4096)));
+//------------------------------------------------------------------------------
+//  TWO STACKS C
+//------------------------------------------------------------------------------
+void two_stacks_c() {
+    // Inicializar al *tope* de cada pila.
+    uintptr_t *a = (uintptr_t*) (stack1+USTACK_SIZE);
+    uintptr_t *b = (uintptr_t*) (stack2+USTACK_SIZE);
+
+    // Preparar, en stack1, la llamada:
+    // vga_write("vga_write() from stack1", 15, 0x57);
+
+    // AYUDA 1: se puede usar alguna forma de pre- o post-
+    // incremento/decremento, según corresponda:
+    //
+    //     *(a++) = ...
+    //     *(++a) = ...
+    //     *(a--) = ...
+    //     *(--a) = ...
+    *(--a) = 0x57;  // color
+    *(--a) = 15;  // linea
+    *(--a) = (uintptr_t) "vga_write() from stack1";  // mensaje
+
+    // AYUDA 2: para apuntar a la cadena con el mensaje,
+    // es suficiente con el siguiente cast:
+    //
+    //   ... a ... = (uintptr_t) "vga_write() from stack1";
+
+    // Preparar, en s2, la llamada:
+    // vga_write("vga_write() from stack2", 16, 0xD0);
+
+    // AYUDA 3: para esta segunda llamada, usar esta forma de
+    // asignación alternativa:
+    b -= 3;
+    b[0] = (uintptr_t) "vga_write() from stack2";
+    b[1] = 16;
+    b[2] = 0xD0;
+
+    // Primera llamada usando task_exec().
+    task_exec((uintptr_t) vga_write, (uintptr_t) a);
+
+    // Segunda llamada con ASM directo. Importante: no
+    // olvidar restaurar el valor de %esp al terminar, y
+    // compilar con: -fasm -fno-omit-frame-pointer.
+    //asm("...; call *%1; ..."
+    asm("movl %0, %%esp; call *%1; movl %%ebp, %%esp"
+    : /* no outputs */
+    : "r"(b), "r"(vga_write));
+}
 //------------------------------------------------------------------------------
 //  KMAIN
 //------------------------------------------------------------------------------
@@ -10,8 +62,7 @@ void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
 
     two_stacks();
-    // vga_write("vga_write() from stack1", 12, 0x17);
-    // vga_write("vga_write() from stack2", 13, 0x90);
+    two_stacks_c();
 
     if (mbi->flags & MULTIBOOT_INFO_CMDLINE) {
         char buf[256] = "cmdline: ";
