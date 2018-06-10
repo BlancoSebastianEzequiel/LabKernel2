76e753ef647b07179edc5437ce304d69992fd3d5
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-10 05:41:08 -0300

    Agrego cambios pero no funciona, hay que saber como configurar el stack2 y no se como hasta el momento

--- contador.c
+++ contador.c
@@ -6,7 +6,7 @@
 #define USTACK_SIZE 4096
 
 static volatile char *const VGABUF = (volatile void *) 0xb8000;
-
+//char msg[256] = "";
 static uintptr_t esp;
 static uint8_t stack1[USTACK_SIZE] __attribute__((aligned(4096)));
 static uint8_t stack2[USTACK_SIZE] __attribute__((aligned(4096)));
@@ -14,8 +14,11 @@ static uint8_t stack2[USTACK_SIZE] __attribute__((aligned(4096)));
 // YIELD
 //------------------------------------------------------------------------------
 static void yield() {
-    if (esp)
+    if (esp) {
+        //fmt_int(esp, msg, 256);
         task_swap(&esp);
+        //vga_write(msg, 22, 0x2F);
+    }
 }
 //------------------------------------------------------------------------------
 // CONTADOR YIELD
@@ -60,6 +63,11 @@ void contador_run() {
     *(--a) = 0;  // linea
     *(--a) = 100;  // numero
 
+    /*
+     la configuración del segundo contador es más compleja, y seguramente sea
+     mejor realizarla tras implementar task_swap(); pues se debe crear
+     artificialmente el stack tal y como lo hubiera preparado esta función.
+    */
     *(--b) = 0x4F;  // color
     *(--b) = 1;  // linea
     *(--b) = 100;  // numero
@@ -67,6 +75,19 @@ void contador_run() {
     // Actualizar la variable estática ‘esp’ para que apunte
     // al del segundo contador.
     esp = (uintptr_t) b;
+
+    // push %esp
+    b--;
+    asm("movl %%esp, %0;": /* no outputs */: "r"(b));
+    // push %ebp
+    b--;
+    asm("movl %%ebp, %0;": /* no outputs */: "r"(b));
+    // push %esi
+    b--;
+    asm("movl %%esi, %0;": /* no outputs */: "r"(b));
+    // push %ebx
+    b--;
+    asm("movl %%ebx, %0;": /* no outputs */: "r"(b));
 
     // Lanzar el primer contador con task_exec.
     task_exec((uintptr_t) contador_yield, (uintptr_t) a);
