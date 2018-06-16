eb48462f9b42b073a19fb0f59317f5efce133884
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-16 00:27:24 -0300

    Ej: kern2-irq: Agrego funciones irq_init(), irq_remap() donde instalo time_asm

--- interrupts.c
+++ interrupts.c
@@ -6,6 +6,8 @@
 #define LOW_MASK 0xFFFF
 #define DESP 16
 
+#define outb(port, data) asm("outb %b0,%w1" : : "a"(data), "d"(port));
+
 static struct IDTR idtr;
 static struct Gate idt[IDTSIZE];
 
@@ -16,7 +18,9 @@ static const uint8_t KSEG_CODE = 8;
 // Identificador de "Interrupt gate de 32 bits" (ver IA32-3A,
 // tabla 6-2: IDT Gate Descriptors).
 static const uint8_t STS_IG32 = 0xE;
-
+//------------------------------------------------------------------------------
+// IDT INIT
+//------------------------------------------------------------------------------
 void idt_init(void){
 
 	// (1) Instalar manejadores ("interrupt service routines").
@@ -27,7 +31,9 @@ void idt_init(void){
 
 	asm("lidt %0" : : "m"(idtr));
 }
-
+//------------------------------------------------------------------------------
+// IDT INSTALL
+//------------------------------------------------------------------------------
 void idt_install(uint8_t n, void (*handler)(void)) {
     uintptr_t addr = (uintptr_t) handler;
 
@@ -40,3 +46,33 @@ void idt_install(uint8_t n, void (*handler)(void)) {
 
     idt[n].present = 1;
 }
+//------------------------------------------------------------------------------
+// IRQ REAMP
+//------------------------------------------------------------------------------
+static void irq_remap() {
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11);
+    outb(0x21, 0x20);
+    outb(0xA1, 0x28);
+    outb(0x21, 0x04);
+    outb(0xA1, 0x02);
+    outb(0x21, 0x01);
+    outb(0xA1, 0x01);
+    outb(0x21, 0x0);
+    outb(0xA1, 0x0);
+}
+//------------------------------------------------------------------------------
+// IRQ INIT
+//------------------------------------------------------------------------------
+void irq_init() {
+    // (1) Redefinir códigos para IRQs.
+    irq_remap();
+
+    // (2) Instalar manejadores.
+    idt_install(T_TIMER, timer_asm);
+    idt_install(T_KEYBOARD, ack_irq);
+
+    // (3) Habilitar interrupciones.
+    asm("sti");
+}
+//------------------------------------------------------------------------------
