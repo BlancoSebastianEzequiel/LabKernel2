3242cbed24036216de66f250907180b77bd2ef48
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-18 15:16:39 -0300

    Agrego conversor de codigo a pdf

--- code.sh
+++ code.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
 ROOT=./
+LIB=lib/
 
 if [[ -e code.md ]]; then
     rm code.md
 fi
 touch code.md
 
-for case in $(ls $ROOT); do
+
+echo "## Libs" >> code.md
+
+for case in $(ls $LIB); do
     filename=$(basename ${case%%.*})
     extension="${case##*.}"
 
     if [[ $extension != 'c'  &&  $extension != 'h' ]]; then
+        continue
+    fi
+
+    string="# $case"   # El numeral en markdown implica titulo
+    echo $string >> code.md
+    echo '```' >> code.md
+    cat $LIB/$case >> code.md
+    echo '  ' >> code.md
+    echo '```' >> code.md
+
+done
+
+echo "## Src" >> code.md
+
+for case in $(ls $ROOT); do
+    filename=$(basename ${case%%.*})
+    extension="${case##*.}"
+
+    if [[ $extension != 'S'  && $extension != 'c'  &&  $extension != 'h' ]]; then
         continue
     fi
 
