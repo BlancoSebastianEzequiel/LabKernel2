07293b76b942447fb781087789f5367731a1c7d7
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-20 00:43:38 -0300

    Agrego definicion de  IF Bandera de interrupción habilitada

--- sched.h
+++ sched.h
@@ -3,6 +3,9 @@
 
 #include "lib/stdint.h"
 
+// IF Bandera de interrupción habilitada (bit 9: 0010 0000 0000)
+#define IF 0x200
+
 enum TaskStatus {
     FREE = 0,
     READY = 1,
@@ -11,8 +14,20 @@ enum TaskStatus {
 };
 
 struct TaskFrame {
-    // Ver ejercicio kern2-spawn más abajo.
-};
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+    /* below here defined by x86 hardware */
+    uint32_t eip;
+    uint16_t cs;
+    uint16_t padding;
+    uint32_t eflags;
+} __attribute__((packed));
 
 struct Task {
     uint8_t stack[4096];
