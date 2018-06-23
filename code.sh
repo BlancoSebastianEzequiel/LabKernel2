982c75cd338d5f841d2a57e8b660647e59e967b4
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2018-06-23 00:27:14 -0300

    Agrego generador de codigo a pdf mejorado

--- code.sh
+++ code.sh
@@ -3,49 +3,35 @@
 ROOT=./
 LIB=lib/
 
+function create_file () {
+    for case in $(ls $1); do
+        filename=$(basename ${case%%.*})
+        extension="${case##*.}"
+
+        if [[ $extension != 'S'  && $extension != 'c'  &&  $extension != 'h' ]];
+        then
+            continue
+        fi
+
+        string="# $case"   # El numeral en markdown implica titulo
+        echo $string >> code.md
+        echo '```' >> code.md
+        cat $1/$case >> code.md
+        echo '  ' >> code.md
+        echo '```' >> code.md
+    done
+}
+
 if [[ -e code.md ]]; then
     rm code.md
 fi
 touch code.md
 
-
 echo "## Libs" >> code.md
-
-for case in $(ls $LIB); do
-    filename=$(basename ${case%%.*})
-    extension="${case##*.}"
-
-    if [[ $extension != 'c'  &&  $extension != 'h' ]]; then
-        continue
-    fi
-
-    string="# $case"   # El numeral en markdown implica titulo
-    echo $string >> code.md
-    echo '```' >> code.md
-    cat $LIB/$case >> code.md
-    echo '  ' >> code.md
-    echo '```' >> code.md
-
-done
-
+create_file $LIB
 echo "## Src" >> code.md
+create_file $ROOT
 
-for case in $(ls $ROOT); do
-    filename=$(basename ${case%%.*})
-    extension="${case##*.}"
-
-    if [[ $extension != 'S'  && $extension != 'c'  &&  $extension != 'h' ]]; then
-        continue
-    fi
-
-    string="# $case"   # El numeral en markdown implica titulo
-    echo $string >> code.md
-    echo '```' >> code.md
-    cat $case >> code.md
-    echo '  ' >> code.md
-    echo '```' >> code.md
-
-done
 
 pandoc code.md -t latex -o code.pdf
 rm code.md
